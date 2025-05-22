026bde71fb38d4a3c42accb1f029355618ae788f
Harley0211 <harleydavevalencia@gmamil.com>
2025-05-22 23:45:36 +0800

    updated code 22/05

--- Components/Header.php
+++ Components/Header.php
@@ -1,10 +1,10 @@
 <div class="header">
-    <div class="name">HARLEY.</div>
+    <div class="name"><a href="Index.php">HARLEY.</a></div>
     <div class="menu">
         <div class="navlink">
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Me</a></li>
+                <li><a href="../Index.php" class="active">Home</a></li>
+                <li><a href="../Pages/About.php">About Me</a></li>
                 <li><a href="#">Projects</a></li>
             </ul>
             <button id="btn-contact">Contact Me</button>
--- Index.php
+++ Index.php
@@ -1,4 +1,3 @@
-<?php include 'Components/Header.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,10 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Styles/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <title>Harley</title>
 </head>
 
 <body>
+    <?php include 'Components/Header.php'; ?>
+
 
     <div class="scroll-container">
         <div class="section hero-section">
@@ -57,14 +59,59 @@
                     <p>A dedicated Junior Full Stack Developer that brings the best version of the clients task and do it professionally. I offer an affordable and skill base rate in order to met the expectation of the clients</p>
                 </div>
                 <div class="button-group">
-                    <button class="button-see-more">About Me</button>
+                    <button class="button-aboutme">About Me</button>
                     <button class="button-projects">Projects</button>
                 </div>
             </div>
 
             <div class="right-side-container">
 
+                <img src="Assets/img-2.png" alt="image2" class="image2">
 
+            </div>
+            <div class="floating-button">
+                <!-- Facebook Button -->
+                <a href="https://m.me/dave.harlskie.02/"
+                    target="_blank"
+                    class="facebook-button"
+                    id="facebook-button"
+                    title="Message me on Facebook">
+
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+                        <g fill="#003764" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                            <g transform="scale(5.12,5.12)">
+                                <path d="M25,3c-12.15,0 -22,9.85 -22,22c0,11.03 8.125,20.137 18.712,21.728v-15.897h-5.443v-5.783h5.443v-3.848c0,-6.371 3.104,-9.168 8.399,-9.168c2.536,0 3.877,0.188 4.512,0.274v5.048h-3.612c-2.248,0 -3.033,2.131 -3.033,4.533v3.161h6.588l-0.894,5.783h-5.694v15.944c10.738,-1.457 19.022,-10.638 19.022,-21.775c0,-12.15 -9.85,-22 -22,-22z"></path>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
+
+                <!-- LinkedIn Button -->
+                <a href="https://www.linkedin.com/in/valencia-harley-dave-93ab15286/" class="linkedin-button" id="linkedin-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+                        <g fill="#003764" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                            <g transform="scale(5.12,5.12)">
+                                <path d="M41,4h-32c-2.76,0 -5,2.24 -5,5v32c0,2.76 2.24,5 5,5h32c2.76,0 5,-2.24 5,-5v-32c0,-2.76 -2.24,-5 -5,-5zM17,20v19h-6v-19zM11,14.47c0,-1.4 1.2,-2.47 3,-2.47c1.8,0 2.93,1.07 3,2.47c0,1.4 -1.12,2.53 -3,2.53c-1.8,0 -3,-1.13 -3,-2.53zM39,39h-6c0,0 0,-9.26 0,-10c0,-2 -1,-4 -3.5,-4.04h-0.08c-2.42,0 -3.42,2.06 -3.42,4.04c0,0.91 0,10 0,10h-6v-19h6v2.56c0,0 1.93,-2.56 5.81,-2.56c3.97,0 7.19,2.73 7.19,8.26z"></path>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
+
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=harleydavevalencia@gmail.com&su=Portfolio%20Inquiry&body=Hi%20Harley%2C%0A%0AI%20saw%20your%20portfolio%20and%20would%20like%20to%20get%20in%20touch%20with%20you."
+                    target="_blank"
+                    class="gmail-button"
+                    id="gmail-button"
+                    title="Email me via Gmail">
+
+
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
+                        <g fill="#003764" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                            <g transform="scale(5.12,5.12)">
+                                <path d="M12,23.403v-0.013v-13.001l-0.12,-0.089h-0.01l-2.73,-2.02c-1.67,-1.24 -4.05,-1.18 -5.53,0.28c-0.99,0.98 -1.61,2.34 -1.61,3.85v3.602zM38,23.39v0.013l10,-7.391v-3.602c0,-1.49 -0.6,-2.85 -1.58,-3.83c-1.46,-1.457 -3.765,-1.628 -5.424,-0.403l-2.876,2.123l-0.12,0.089zM14,24.868l10.406,7.692c0.353,0.261 0.836,0.261 1.189,0l10.405,-7.692v-13.001l-11,8.133l-11,-8.133zM38,25.889v15.111c0,0.552 0.448,1 1,1h6.5c1.381,0 2.5,-1.119 2.5,-2.5v-21.003zM12,25.889l-10,-7.392v21.003c0,1.381 1.119,2.5 2.5,2.5h6.5c0.552,0 1,-0.448 1,-1z"></path>
+                            </g>
+                        </g>
+                    </svg>
+                </a>
             </div>
 
         </div>
@@ -106,6 +153,20 @@
             icons.style.opacity = opacity;
             hero.querySelector('.intro-text').style.opacity = opacity;
             hero.querySelector('.image1').style.opacity = opacity;
+        });
+
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                }
+            });
+        }, {
+            threshold: 0.2
+        });
+
+        document.querySelectorAll('.section').forEach(section => {
+            observer.observe(section);
         });
     </script>
 </body>
